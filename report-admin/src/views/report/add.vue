93931ead096b0cd862dcf0fda841c7cd0cd92af4
Guiyun <i@guiyunweb.com>
2020-10-30 10:47:02 +0800

    init

---
+++ report-admin/src/views/report/add.vue
@@ -0,0 +1,12 @@
+<template>
+  <d2-container>
+    <template slot="header">添加报表</template>
+    添加报表
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'add'
+}
+</script>
---
+++ report-admin/src/views/report/find.vue
@@ -0,0 +1,12 @@
+<template>
+  <d2-container>
+    <template slot="header">报表查询</template>
+    报表查询
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'find'
+}
+</script>
